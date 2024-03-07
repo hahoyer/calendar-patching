423b0d7be03565e8f8f42ac4bb09a8b5a1a56cd2
Harald Herbert Hoyer <hahoyer@gmail.com>
2024-03-07 09:23:37 +0100

    initial

---
+++ source/dev/Program.cs
@@ -0,0 +1,18 @@
+﻿using hw.Helper;
+using Ical.Net;
+using Ical.Net.Serialization;
+
+var icsFile = SmbFile
+    .Select("q:\\download\\lernwerk*.ics")
+    .Select(n => n.ToSmbFile())
+    .OrderByDescending(f => f.ModifiedDate)
+    .First();
+
+var calendar = Calendar.Load(icsFile.String);
+var events = calendar.Events.Where(@event => @event.Description.Trim() != "").ToArray();
+calendar.Events.Clear();
+calendar.Events.AddRange(events);
+
+var serializer = new CalendarSerializer(new SerializationContext());
+icsFile.String = serializer.SerializeToString(calendar);
+return;
